12ee8ce0f54173ef61b4f3bd87b2abf1229bb4a9
carlosprietobarron <carlosprietobarron@gmail.com>
2021-01-07 15:51:20 -0600

    Initialize project calculator

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css'
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>MILESTONE 1 CALCULATOR APP</h1>
+      </header>
+    </div>
+  )
+}
+
+export default App

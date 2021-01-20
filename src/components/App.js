4a92b4f8894e9e63f3589b03e86b21ad86696440
carlosprietobarron <carlosprietobarron@gmail.com>
2021-01-20 15:33:59 -0600

    Refactoring: Change class componet APP to functional comp

--- src/components/App.js
+++ src/components/App.js
@@ -1,58 +1,55 @@
 import '../App.css';
-import React, { Component } from 'react';
+import React, { useState } from 'react';
 import Display from './display';
 import ButtonPanel from './btnPanel';
 import calculate from '../logic/calculate';
 
-class App extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-      point: false,
-    };
-    this.handleClick = this.handleClick.bind(this);
-  }
+function App() {
+  const [opObj, setopObj] = useState({
+    total: null,
+    next: null,
+    operation: null,
+    point: false,
+  });
 
-  handleClick(buttonName) {
-    this.sendToCalc(buttonName);
-  }
-
-  sendToCalc(buttonName) {
-    const {
-      total, next, operation, point,
-    } = this.state;
-    const calculator = calculate({
-      total, next, operation, point,
-    }, buttonName);
-    this.updateState(calculator);
-  }
-
-  updateState(calculator) {
-    this.setState({
+  const updateState = calculator => {
+    setopObj({
       total: calculator.total,
       next: calculator.next,
       operation: calculator.operation,
       point: calculator.point,
     });
-  }
+  };
 
-  render() {
-    let { total, next } = this.state;
-    if (!total) total = 0;
-    if (!next) next = 0;
-    if (next !== 0) total = next;
-    return (
-      <>
-        <div className="calc-ui">
-          <Display result={total} />
-          <ButtonPanel updateApp={this.handleClick} />
-        </div>
-      </>
+  const handleClick = buttonName => {
+    const {
+      total, next, operation, point,
+    } = opObj;
+    const calculator = calculate(
+      {
+        total,
+        next,
+        operation,
+        point,
+      },
+      buttonName,
     );
-  }
+    updateState(calculator);
+  };
+
+  let { total, next } = opObj;
+  if (!total) total = 0;
+  if (!next) next = 0;
+  if (next !== 0) total = next;
+
+  return (
+    <>
+      <div className="calc-ui">
+        <Display result={total} />
+        <ButtonPanel updateApp={handleClick} />
+      </div>
+    </>
+  );
 }
 
 export default App;
